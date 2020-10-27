1d90a1ef50ecd556eee3a4eeac5e5e77e36897d4
Diego Osorio <juan.osorio1599@alumnos.udg.mx>
2020-10-27 13:11:32 -0600

    Se crea la funcion menu()

--- main.go
+++ main.go
@@ -1,11 +1,67 @@
 package main
 
-import "./multimedia"
+import (
+	"multimedia"
+	"fmt"
+)
+
+func menu () {
+	var opcion int
+	var nombre, formato, extra string
+
+	fmt.Println("1.- Agregar Imagen")
+	fmt.Println("2.- Agregar Audio")
+	fmt.Println("3.- Agregar Video")
+
+	fmt.Scanln(&opcion)
+
+	switch opcion {
+
+	case 1:
+
+		fmt.Println("Dame el nombre: ")
+		fmt.Scanln(&nombre)
+
+		fmt.Println("Dame el formato: ")
+		fmt.Scanln(&formato)
+
+		fmt.Println("Dame el numero de canales: ")
+		fmt.Scanln(&extra)
+
+		img := multimedia.Imagen{nombre,formato,extra}
+		contenidoweb = append(img)
+
+	case 2:
+
+		fmt.Println("Dame el nombre: ")
+		fmt.Scanln(&nombre)
+
+		fmt.Println("Dame el formato: ")
+		fmt.Scanln(&formato)
+
+		fmt.Println("Dame el numero de duracion: ")
+		fmt.Scanln(&extra)
+
+
+	case 3:
+
+		fmt.Println("Dame el nombre: ")
+		fmt.Scanln(&nombre)
+
+		fmt.Println("Dame el formato: ")
+		fmt.Scanln(&formato)
+
+		fmt.Println("Dame el numero de frames: ")
+		fmt.Scanln(&extra)
+
+
+	case 4:
+		
+	}
+}
+
 func main ()  {
-
-	img1 := multimedia.Imagen{"imagen1","jpg","4"}
-	aud1 := multimedia.Audio{"Audio1","Mp3","10.5"}
-
-	img1.Mostrar()
-	aud1.Mostrar()
+	contenidoweb := multimedia.ContenidoWeb{}
+	menu()
+	
 }
